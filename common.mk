e55039d0a9d6e56be7e4428f616915b3793bd7c3
gr8nole <davemartinfsu@gmail.com>
2016-08-19 07:18:33 -0400

    don't prefer tvdpi

--- common.mk
+++ common.mk
@@ -19,8 +19,8 @@ COMMON_PATH := device/samsung/lt01-common
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(COMMON_PATH)/overlay
 
-PRODUCT_AAPT_CONFIG := normal large
-PRODUCT_AAPT_PREF_CONFIG := tvdpi
+PRODUCT_AAPT_CONFIG := normal large hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
 PRODUCT_AAPT_PREBUILT_DPI := hdpi
 
 # Boot animation
